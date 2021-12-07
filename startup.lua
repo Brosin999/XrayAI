76eb4cebc9191af248250b14307f92fde39d9e07
= <=>
2021-12-06 23:48:19 -0900

    updated startup

--- startup.lua
+++ startup.lua
@@ -1,3 +1,19 @@
+shell.run("mkdir lib")
+shell.run("cd lib")
+shell.run("wget https://raw.githubusercontent.com/GuitarMusashi616/XrayAI/main/lib/class.lua")
+shell.run("wget https://raw.githubusercontent.com/GuitarMusashi616/XrayAI/main/lib/list.lua")
+shell.run("wget https://raw.githubusercontent.com/GuitarMusashi616/XrayAI/main/lib/tbl.lua")
+shell.run("wget https://raw.githubusercontent.com/GuitarMusashi616/XrayAI/main/lib/tsp.lua")
+shell.run("wget https://raw.githubusercontent.com/GuitarMusashi616/XrayAI/main/lib/util.lua")
+shell.run("cd ..")
+shell.run("mkdir cc")
+shell.run("cd cc")
+shell.run("wget https://raw.githubusercontent.com/GuitarMusashi616/XrayAI/main/cc/turtle.lua")
+shell.run("cd ..")
+shell.run("wget https://raw.githubusercontent.com/GuitarMusashi616/XrayAI/main/startup.lua")
+shell.run("wget https://raw.githubusercontent.com/GuitarMusashi616/XrayAI/main/turtleAI.lua")
+shell.run("wget https://raw.githubusercontent.com/GuitarMusashi616/XrayAI/main/xray_pathfind.lua")
+
 local TRAVEL_ANCHOR_SLOT = 14
 
 turtle.select(TRAVEL_ANCHOR_SLOT)
